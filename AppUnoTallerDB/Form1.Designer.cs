0d108791dab9f99df027382fb7bd938053362f92
Edgar Mijares <progresssystemsoftware@gmail.com>
2019-03-25 15:53:37 -0600

    Init

---
+++ AppUnoTallerDB/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace AppUnoTallerDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nombre_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.apellidos_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calle_txt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.noext_txt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.poblacion_txt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pais_txt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.fechanac_txt = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.curp_txt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numcontrol_txt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.escuelaprocede_txt = new System.Windows.Forms.TextBox();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nombre_txt
+            // 
+            this.nombre_txt.AccessibleDescription = "nombre_txt";
+            this.nombre_txt.AccessibleName = "nombre_txt";
+            this.nombre_txt.Location = new System.Drawing.Point(31, 40);
+            this.nombre_txt.Name = "nombre_txt";
+            this.nombre_txt.Size = new System.Drawing.Size(140, 20);
+            this.nombre_txt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Apellidos";
+            // 
+            // apellidos_txt
+            // 
+            this.apellidos_txt.AccessibleName = "apellidos_txt";
+            this.apellidos_txt.Location = new System.Drawing.Point(178, 40);
+            this.apellidos_txt.Name = "apellidos_txt";
+            this.apellidos_txt.Size = new System.Drawing.Size(239, 20);
+            this.apellidos_txt.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Calle";
+            // 
+            // calle_txt
+            // 
+            this.calle_txt.AccessibleName = "calle_txt";
+            this.calle_txt.Location = new System.Drawing.Point(31, 89);
+            this.calle_txt.Name = "calle_txt";
+            this.calle_txt.Size = new System.Drawing.Size(140, 20);
+            this.calle_txt.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(175, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "No. Exterior";
+            // 
+            // noext_txt
+            // 
+            this.noext_txt.AccessibleName = "noext_txt";
+            this.noext_txt.Location = new System.Drawing.Point(178, 89);
+            this.noext_txt.Name = "noext_txt";
+            this.noext_txt.Size = new System.Drawing.Size(115, 20);
+            this.noext_txt.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(296, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Poblacion";
+            // 
+            // poblacion_txt
+            // 
+            this.poblacion_txt.AccessibleName = "poblacion_txt";
+            this.poblacion_txt.Location = new System.Drawing.Point(299, 89);
+            this.poblacion_txt.Name = "poblacion_txt";
+            this.poblacion_txt.Size = new System.Drawing.Size(118, 20);
+            this.poblacion_txt.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Pais";
+            // 
+            // pais_txt
+            // 
+            this.pais_txt.AccessibleName = "pais_txt";
+            this.pais_txt.Location = new System.Drawing.Point(31, 138);
+            this.pais_txt.Name = "pais_txt";
+            this.pais_txt.Size = new System.Drawing.Size(140, 20);
+            this.pais_txt.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(174, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Fecha de nacimieto";
+            // 
+            // fechanac_txt
+            // 
+            this.fechanac_txt.AccessibleName = "fechanac_txt";
+            this.fechanac_txt.Location = new System.Drawing.Point(178, 138);
+            this.fechanac_txt.Name = "fechanac_txt";
+            this.fechanac_txt.Size = new System.Drawing.Size(239, 20);
+            this.fechanac_txt.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "CURP";
+            // 
+            // curp_txt
+            // 
+            this.curp_txt.AccessibleName = "curp_txt";
+            this.curp_txt.Location = new System.Drawing.Point(31, 195);
+            this.curp_txt.Name = "curp_txt";
+            this.curp_txt.Size = new System.Drawing.Size(185, 20);
+            this.curp_txt.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(236, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "No Control";
+            // 
+            // numcontrol_txt
+            // 
+            this.numcontrol_txt.AccessibleName = "numcontrol_txt";
+            this.numcontrol_txt.Location = new System.Drawing.Point(232, 195);
+            this.numcontrol_txt.Name = "numcontrol_txt";
+            this.numcontrol_txt.Size = new System.Drawing.Size(185, 20);
+            this.numcontrol_txt.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AccessibleName = "";
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(28, 228);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Escuela procedencia";
+            // 
+            // escuelaprocede_txt
+            // 
+            this.escuelaprocede_txt.AccessibleName = "escuelaprocede_txt";
+            this.escuelaprocede_txt.Location = new System.Drawing.Point(31, 244);
+            this.escuelaprocede_txt.Name = "escuelaprocede_txt";
+            this.escuelaprocede_txt.Size = new System.Drawing.Size(386, 20);
+            this.escuelaprocede_txt.TabIndex = 19;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.AccessibleDescription = "btn_guardar";
+            this.btn_guardar.AccessibleName = "btn_guardar";
+            this.btn_guardar.Location = new System.Drawing.Point(31, 279);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(185, 23);
+            this.btn_guardar.TabIndex = 21;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.AccessibleName = "btn_limpiar";
+            this.btn_limpiar.Location = new System.Drawing.Point(232, 279);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(185, 23);
+            this.btn_limpiar.TabIndex = 22;
+            this.btn_limpiar.Text = "limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 328);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.escuelaprocede_txt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numcontrol_txt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.curp_txt);
+            this.Controls.Add(this.fechanac_txt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pais_txt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.poblacion_txt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.noext_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calle_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.apellidos_txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nombre_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nombre_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox apellidos_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox calle_txt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox noext_txt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox poblacion_txt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox pais_txt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker fechanac_txt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox curp_txt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox numcontrol_txt;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox escuelaprocede_txt;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_limpiar;
+    }
+}
+
